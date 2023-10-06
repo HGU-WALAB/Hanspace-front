bc1eafb49794b467542109fe24b419274489667f
healim01 <22000770@handong.edu>
2023-10-06 23:13:30 +0900

    feat: space card

--- src/types/tour.ts
+++ src/types/tour.ts
@@ -29,22 +29,22 @@ export type ITourBooker = {
 export type ITourItem = {
   id: string;
   name: string;
-  price: number;
-  totalViews: number;
-  tags: string[];
-  content: string;
-  publish: string;
-  images: string[];
-  durations: string;
-  priceSale: number;
-  services: string[];
-  destination: string;
-  ratingNumber: number;
-  bookers: ITourBooker[];
-  tourGuides: ITourGuide[];
-  createdAt: Date;
-  available: {
-    startDate: Date;
-    endDate: Date;
-  };
+  // price: number;
+  // totalViews: number;
+  // tags: string[];
+  // content: string;
+  // publish: string;
+  images: string;
+  // durations: string;
+  // priceSale: number;
+  // services: string[];
+  // destination: string;
+  // ratingNumber: number;
+  // bookers: ITourBooker[];
+  // tourGuides: ITourGuide[];
+  // createdAt: Date;
+  // available: {
+  //   startDate: Date;
+  //   endDate: Date;
+  // };
 };
