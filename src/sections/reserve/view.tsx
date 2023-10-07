b276a6f9bfdd8fd498936e4bc5f4e0469961524a
LeeHannaa <tiger5391@naver.com>
2023-10-07 19:42:18 +0900

    feat: reserve-start

--- src/sections/reserve/view.tsx
+++ src/sections/reserve/view.tsx
@@ -3,19 +3,30 @@ import { alpha } from '@mui/material/styles';
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
 // components
 import { useSettingsContext } from 'src/components/settings';
+import { CalendarView } from 'src/sections/calendar/view';
+// react
+import { useState } from 'react';
 
 // ----------------------------------------------------------------------
 
 export default function ReserveView() {
   const settings = useSettingsContext();
+  const [showCalendar, setShowCalendar] = useState(false);
+
+  const toggleCalendar = () => {
+    setShowCalendar(!showCalendar);
+  };
 
   return (
     <Container maxWidth={settings.themeStretch ? false : 'xl'}>
-      <Typography variant="h4"> Page Reserve </Typography>
+      <Typography variant="h4"> 대여 가능한 공간 </Typography>
+      <Button onClick={toggleCalendar} variant="outlined"> Calendar</Button>
+      {showCalendar && <CalendarView />}
 
-      <Box
+      {/* <Box
         sx={{
           mt: 5,
           width: 1,
@@ -24,7 +35,8 @@ export default function ReserveView() {
           bgcolor: (theme) => alpha(theme.palette.grey[500], 0.04),
           border: (theme) => `dashed 1px ${theme.palette.divider}`,
         }}
-      />
+      /> */}
+      
     </Container>
   );
 }
