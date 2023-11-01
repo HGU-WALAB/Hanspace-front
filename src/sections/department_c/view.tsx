144a24ce1627bf723a995dcf5802ac1d4d838f60
LeeHannaa <tiger5391@naver.com>
2023-11-02 02:47:17 +0900

    feat: dept_c form update

--- src/sections/department_c/view.tsx
+++ src/sections/department_c/view.tsx
@@ -1,5 +1,7 @@
 // @mui
 import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import { alpha } from '@mui/material/styles';
 
 import DepartmentForm from './dept-form';
 
@@ -7,10 +9,21 @@ export default function DepartmentView() {
 
   return (
     <>
-    <p>기관 등록 폼 페이지</p>
-    <Box display="flex" flexDirection="column" alignItems="center">
+    <Typography variant="h5"> 기관 추가 신청하기 </Typography>
+    {/* <Box
+        sx={{
+          mt: 5,
+          width: 0.5,
+          borderRadius: 2,
+          bgcolor: (theme) => alpha(theme.palette.grey[500], 0.04),
+          // border: (theme) => `dashed 1px ${theme.palette.divider}`,
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}
+      > */}
         <DepartmentForm />
-    </Box>
+      {/* </Box> */}
     </>
   );
 
