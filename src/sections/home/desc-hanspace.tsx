e499251e90d059f928b8d697d2d1bc160d661d54
Hyelim Choi <HyelimChoi01@gmail.com>
2023-11-06 02:40:41 +0900

    feat: side menu bar

--- src/sections/home/desc-hanspace.tsx
+++ src/sections/home/desc-hanspace.tsx
@@ -1,12 +1,10 @@
 // @mui
 import { Box } from '@mui/material';
-import Typography from '@mui/material/Typography';
 
 export default function DescHanspace() {
   return (
-    <Box>
-      <Typography variant="h4"> Hanspace </Typography>
-      <Typography variant="h4"> 설명 사진 첨부 </Typography>
+    <Box style={{ padding: '24px' }}>
+      <Box component="img" src="/assets/images/main/hanspaceDesc.svg" style={{ width: '100%' }} />
     </Box>
   );
 }
