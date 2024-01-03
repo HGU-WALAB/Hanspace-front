f2e73372f686e0e17eb8b03a937ae7e161f70cc8
Hyelim Choi <HyelimChoi01@gmail.com>
2024-01-04 04:59:55 +0900

    feat: set page divider with role

--- src/pages/dashboard/dashboard.tsx
+++ src/pages/dashboard/dashboard.tsx
@@ -1,18 +1,24 @@
 import { Helmet } from 'react-helmet-async';
+import { useRecoilValue } from 'recoil';
 // sections
 import AdminDashboardView from 'src/sections/dashboard/admin-view';
 import UserDashboardView from 'src/sections/dashboard/user-view';
+import { userDeptState } from 'src/utils/atom';
 // ----------------------------------------------------------------------
 
 export default function Page() {
+  const userDeptInfo = useRecoilValue(userDeptState);
   return (
     <>
       <Helmet>
         <title> HANSPACE: 대시보드 </title>
       </Helmet>
-
-      <AdminDashboardView />
-      {/* <UserDashboardView /> */}
+      {typeof userDeptInfo === 'object' &&
+      userDeptInfo.deptMemberResponse[0].deptRole === 'Admin' ? (
+        <AdminDashboardView />
+      ) : (
+        <UserDashboardView />
+      )}
     </>
   );
 }
