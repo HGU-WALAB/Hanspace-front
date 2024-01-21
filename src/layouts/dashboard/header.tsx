81fccea82eb3fd8bc8d11edfc1f6bf7642bf87ab
Hyelim Choi <HyelimChoi01@gmail.com>
2024-01-21 21:16:53 +0900

    delete: notifications popover

--- src/layouts/dashboard/header.tsx
+++ src/layouts/dashboard/header.tsx
@@ -66,7 +66,7 @@ export default function Header({ onOpenNav }: Props) {
 
         <SettingsButton />
 
-        <NotificationsPopover />
+        {/* <NotificationsPopover /> */}
 
         {/* <ContactsPopover /> */}
 
