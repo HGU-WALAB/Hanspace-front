baf69fcf47291dc6b91cbb8985f85f1d07b5dbd2
LeeHannaa <103620720+LeeHannaa@users.noreply.github.com>
2024-01-25 16:37:50 +0900

    feat: reserve filtering

--- src/sections/reserve/view.tsx
+++ src/sections/reserve/view.tsx
@@ -30,195 +30,6 @@ import ReserveCSVForm from './reserve-csv';
 import DailyReserveFormDialog from './reserve-daily-dialog';
 import RegularlyReserveDialog from './reserve-regularly-dialog';
 
-// --------더미데이터-----------
-const datereservelist = [
-  {
-    regDate: '2024-01-08T15:25:34.529738',
-    modDate: '2024-01-20T21:05:29.198985',
-    id: 2,
-    reserveDate: '2023-08-18',
-    startTime: '15:00',
-    endTime: '16:00',
-    headCount: 4,
-    purpose: 'study',
-    status: '거절',
-    extraInfoAns: '전산전자공학부, 김광교수님',
-    invitedMemberEmail: '22000631@handong.ac.kr',
-    createMemberName: '장유진',
-    space: {
-      regDate: '2024-01-08T15:24:49.738147',
-      modDate: '2024-01-18T20:47:09.330652',
-      spaceId: 1,
-      name: ' 뉴턴홀 317',
-      headCount: 13,
-      availableStart: '10:00',
-      availableEnd: '16:00',
-      detail: '23',
-      availability: true,
-      image: 'IMG_3579.jpeg',
-      department: {
-        regDate: '2023-11-27T19:01:40.647382',
-        modDate: '2024-01-23T20:41:24.106982',
-        deptId: 1,
-        siteName: 'ICT창업학부',
-        deptName: 'ICT창업학부',
-        userAccept: false,
-        maxReserveCount: 50,
-        link: 'ICT',
-        extraInfo: '학부, 교수이름',
-      },
-    },
-  },
-  {
-    regDate: '2024-01-16T22:10:33.383726',
-    modDate: '2024-01-20T20:39:51.844411',
-    id: 13,
-    reserveDate: '2023-08-18',
-    startTime: '13:00',
-    endTime: '14:00',
-    headCount: 4,
-    purpose: 'study',
-    status: '승인',
-    extraInfoAns: '전산전자공학부, 김광교수님',
-    invitedMemberEmail: '22000631@handong.ac.kr',
-    createMemberName: '장유진',
-    space: {
-      regDate: '2024-01-08T15:24:49.738147',
-      modDate: '2024-01-18T20:47:09.330652',
-      spaceId: 1,
-      name: ' 뉴턴홀 317',
-      headCount: 13,
-      availableStart: '10:00',
-      availableEnd: '16:00',
-      detail: '23',
-      availability: true,
-      image: 'IMG_3579.jpeg',
-      department: {
-        regDate: '2023-11-27T19:01:40.647382',
-        modDate: '2024-01-23T20:41:24.106982',
-        deptId: 1,
-        siteName: 'ICT창업학부',
-        deptName: 'ICT창업학부',
-        userAccept: false,
-        maxReserveCount: 50,
-        link: 'ICT',
-        extraInfo: '학부, 교수이름',
-      },
-    },
-  },
-  {
-    regDate: '2024-01-16T22:22:10.168776',
-    modDate: '2024-01-20T20:55:02.775091',
-    id: 14,
-    reserveDate: '2023-08-18',
-    startTime: '11:00',
-    endTime: '12:00',
-    headCount: 4,
-    purpose: 'study',
-    status: '거절',
-    extraInfoAns: '전산전자공학부, 김광교수님',
-    invitedMemberEmail: '22000631@handong.ac.kr',
-    createMemberName: '이한나',
-    space: {
-      regDate: '2024-01-08T15:24:49.738147',
-      modDate: '2024-01-18T20:47:09.330652',
-      spaceId: 1,
-      name: ' 뉴턴홀 317',
-      headCount: 13,
-      availableStart: '10:00',
-      availableEnd: '16:00',
-      detail: '23',
-      availability: true,
-      image: 'IMG_3579.jpeg',
-      department: {
-        regDate: '2023-11-27T19:01:40.647382',
-        modDate: '2024-01-23T20:41:24.106982',
-        deptId: 1,
-        siteName: 'ICT창업학부',
-        deptName: 'ICT창업학부',
-        userAccept: false,
-        maxReserveCount: 50,
-        link: 'ICT',
-        extraInfo: '학부, 교수이름',
-      },
-    },
-  },
-  {
-    regDate: '2024-01-19T22:36:36.496559',
-    modDate: '2024-01-19T22:36:36.496559',
-    id: 15,
-    reserveDate: '2023-08-18',
-    startTime: '10:00',
-    endTime: '11:00',
-    headCount: 4,
-    purpose: 'study',
-    status: '미승인',
-    extraInfoAns: '전산전자공학부, 김광교수님',
-    invitedMemberEmail: '22000631@handong.ac.kr',
-    createMemberName: '이한나',
-    space: {
-      regDate: '2024-01-08T15:24:49.738147',
-      modDate: '2024-01-18T20:47:09.330652',
-      spaceId: 1,
-      name: ' 뉴턴홀 317',
-      headCount: 13,
-      availableStart: '10:00',
-      availableEnd: '16:00',
-      detail: '23',
-      availability: true,
-      image: 'IMG_3579.jpeg',
-      department: {
-        regDate: '2023-11-27T19:01:40.647382',
-        modDate: '2024-01-23T20:41:24.106982',
-        deptId: 1,
-        siteName: 'ICT창업학부',
-        deptName: 'ICT창업학부',
-        userAccept: false,
-        maxReserveCount: 50,
-        link: 'ICT',
-        extraInfo: '학부, 교수이름',
-      },
-    },
-  },
-  {
-    regDate: '2024-01-19T22:37:59.690197',
-    modDate: '2024-01-19T22:37:59.690197',
-    id: 16,
-    reserveDate: '2023-08-18',
-    startTime: '12:00',
-    endTime: '12:30',
-    headCount: 4,
-    purpose: 'study',
-    status: '미승인',
-    extraInfoAns: '전산전자공학부, 김광교수님',
-    invitedMemberEmail: '22000631@handong.ac.kr',
-    createMemberName: '이한나',
-    space: {
-      regDate: '2024-01-19T22:29:40.975794',
-      modDate: '2024-01-19T22:29:40.975794',
-      spaceId: 9,
-      name: '뉴턴홀 110호',
-      headCount: 30,
-      availableStart: '10:00',
-      availableEnd: '16:00',
-      detail: '빔프로젝트 가능',
-      availability: true,
-      labelColor: 'blue',
-      department: {
-        regDate: '2023-11-27T19:01:40.647382',
-        modDate: '2024-01-23T20:41:24.106982',
-        deptId: 1,
-        siteName: 'ICT창업학부',
-        deptName: 'ICT창업학부',
-        userAccept: false,
-        maxReserveCount: 50,
-        link: 'ICT',
-        extraInfo: '학부, 교수이름',
-      },
-    },
-  },
-];
-
 export default function ReserveView() {
   const userDeptValue = useRecoilValue(userDeptState);
 
@@ -247,34 +58,19 @@ export default function ReserveView() {
       },
     }
   );
-  // 기관 id와 날짜에 해당하는 예약 정보들 확인
-  // ToDo: API 수정 시 정보 받고 예약 수정
-  // const { data: reserveds } = useQuery<IReservedItem[]>(
-  //   ['reservedListByDate', reservedListByDate],
-  //   async () =>
-  //     reservedListByDate(deptId, selectedDailyData1.reserveDate.toISOString().split('T')[0]).then(
-  //       (response) => response.data
-  //     ),
-  //   {
-  //     onSuccess: (data) => {
-  //       data.forEach((reserved) => {
-  //         console.log('reservedListByDate', reserved);
-  //       });
-  //     },
-  //   }
-  // );
+
   // ToDo: 더미데이터로 확인중
-  const [reserveDatesList, setReserveDatesList] = useState<IReservedItem[]>([]);
-  useEffect(() => {
-    // datereservelist가 변경될 때마다 실행
-    const updatedReserveDates = datereservelist.map((reserve) => ({
-      spaceId: reserve.space.spaceId,
-      startTime: reserve.startTime,
-      endTime: reserve.endTime,
-    }));
-    setReserveDatesList(updatedReserveDates);
-    console.log('reservedListByDate', updatedReserveDates);
-  }, []);
+  // const [reserveDatesList, setReserveDatesList] = useState<IReservedItem[]>([]);
+  // useEffect(() => {
+  //   // datereservelist가 변경될 때마다 실행
+  //   const updatedReserveDates = datereservelist.map((reserve) => ({
+  //     spaceId: reserve.space.spaceId,
+  //     startTime: reserve.startTime,
+  //     endTime: reserve.endTime,
+  //   }));
+  //   setReserveDatesList(updatedReserveDates);
+  //   console.log('reservedListByDate', updatedReserveDates);
+  // }, []);
 
   // 더미데이터로 확인 중
 
@@ -337,6 +133,46 @@ export default function ReserveView() {
     setIsDialogOpen(true);
     setSelectedRegularyData2(data); // 예약을 위한 모든 정보 선택
   };
+
+  // 기관 id와 날짜에 해당하는 예약 정보들 확인
+  // ToDo: API 수정 시 정보 받고 예약 수정
+  const [reserveDatesList, setReserveDatesList] = useState<any[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await reservedListByDate(
+          deptId,
+          selectedDailyData1.reserveDate.toISOString().split('T')[0]
+        );
+
+        setReserveDatesList(response.data);
+
+        const updatedReserveDates = reserveDatesList.map((reserve) => {
+          if (reserve && typeof reserve === 'object') {
+            console.log(' 예약된 정보들 확인', reserve);
+            return {
+              spaceId: reserve.space.spaceId,
+              startTime: reserve.startTime,
+              endTime: reserve.endTime,
+            };
+          }
+          return reserve; // return the original value if it's not an object
+        });
+
+        setReserveDatesList(updatedReserveDates);
+        // console.log('reservedListByDate', updatedReserveDates);
+      } catch (error) {
+        // Handle error appropriately (e.g., show an error message)
+        console.error('Error fetching reserved items:', error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    // ToDo: fetch 무한...
+    // fetchData();
+  }, [deptId, selectedDailyData1.reserveDate, reserveDatesList]);
 
   let DailySpaceCradList = null;
 
